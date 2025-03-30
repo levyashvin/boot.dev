fef2a9628d60bae6c8d475d5cd41b805d5b8f266
guava <yuvayashvin@gmail.com>
2025-03-30 19:08:17 +0530

    Started python section

---
+++ Learn to Code in Python/fantasy_quest.py
@@ -0,0 +1,16 @@
+#Let's build a game
+print("Welcome to Fantasy Quest!")
+
+#Fix your first bug
+sword_damage = 10
+start_health = 100
+end_health = start_health + sword_damage
+
+# Don't touch below this line
+print(f"Sam's health is: {start_health}")
+print(f"Sam takes {sword_damage} damage...")
+print(f"Sam's health is: {end_health}")
+
+#The console
+print("Please move forward")
+
