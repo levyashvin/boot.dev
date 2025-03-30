7a413aad46fd8d38abdd08c085b5a4e1721e0f7f
guava <yuvayashvin@gmail.com>
2025-03-30 22:01:44 +0530

    Ch5 lesson 3

---
+++ Learn to Code in Python/fantasy_quest/ch4_scope.py
@@ -0,0 +1,33 @@
+#1
+def get_max_health(modifier, level):
+    return modifier * level
+
+
+my_modifier = 5
+my_level = 10
+
+## don't touch above this line
+
+max_health = get_max_health(my_modifier, my_level)
+
+# don't touch below this line
+
+print(f"max_health is: {max_health}")
+
+#2
+player_level = 4
+
+# Don't touch below this line
+
+
+def calculate_health(modifier):
+    return player_level * modifier
+
+
+def calculate_primary_stats(armor_bonus, modifier):
+    return armor_bonus + modifier + player_level
+
+
+print(f"Character has {calculate_health(10)} max health.")
+
+print(f"Character has {calculate_primary_stats(3, 8)} primary stats.")
