23ad2f61e0b0ca253ebd7765a1094be6fbbcbc8d
guava <yuvayashvin@gmail.com>
2025-03-31 17:53:47 +0530

    starting ch6, computing

--- Learn to Code in Python/fantasy_quest/ch5_tests_debuging.py
+++ Learn to Code in Python/fantasy_quest/ch5_tests_debuging.py
@@ -71,3 +71,147 @@ if "__RUN__" in globals():
     test_cases = run_cases
 
 main()
+
+'''
+Assignment
+
+Complete the take_magic_damage function. It should return the new health after calculating how much magic-type damage the player takes. Here is a description of the arguments:
+
+    health: The player's starting health
+    resist: The player's magic resistance. This reduces the damage they take by a static amount
+    amp: The attacker's magic amplification. This increases the damage they deal by a multiplier
+    spell_power: The base damage of the spell
+
+First, calculate the total maximum damage to be inflicted by multiplying the spell_power by the amp. Then, subtract the resist from the total damage to get the actual damage dealt. Apply that damage to the player's health and return the new health.
+'''
+
+def take_magic_damage(health, resist, amp, spell_power):
+    total_dmg = spell_power * amp
+    total_dmg -= resist
+    new_health = health - total_dmg
+    return new_health
+
+run_cases = [
+    (100, 5, 2, 20, 65),
+    (200, 10, 1, 25, 185),
+]
+
+submit_cases = run_cases + [
+    (0, 0, 0, 0, 0),
+    (1, 1, 1, 1, 1),
+    (100, 2, 3, 1, 99),
+    (2500, 3, 2, 2, 2499),
+]
+
+
+def test(input1, input2, input3, input4, expected_output):
+    print("---------------------------------")
+    print(f"Inputs: {input1}, {input2}, {input3}, {input4}")
+    print(f"Expecting: {expected_output}")
+    result = take_magic_damage(input1, input2, input3, input4)
+    print(f"Actual: {result}")
+    if result == expected_output:
+        print("Pass")
+        return True
+    print("Fail")
+    return False
+
+
+def main():
+    passed = 0
+    failed = 0
+    skipped = len(submit_cases) - len(test_cases)
+    for test_case in test_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    if skipped > 0:
+        print(f"{passed} passed, {failed} failed, {skipped} skipped")
+    else:
+        print(f"{passed} passed, {failed} failed")
+
+
+test_cases = submit_cases
+if "__RUN__" in globals():
+    test_cases = run_cases
+
+main()
+
+'''
+Assignment
+
+Let's complete the unlock_achievement function. It accepts 3 arguments:
+
+    before_xp: int
+    ach_xp: int
+    ach_name: str
+
+It should return 2 values:
+
+    The player's xp after the achievement is unlocked (The sum of before_xp and ach_xp)
+    An alert message that says "Achievement Unlocked: ACHIEVEMENT_NAME", where ACHIEVEMENT_NAME is the name of the achievement
+
+'''
+
+def unlock_achievement(before_xp, ach_xp, ach_name):
+    new_xp = before_xp + ach_xp
+    alert_message = "Achievement Unlocked: " + ach_name
+    return new_xp, alert_message
+
+run_cases = [
+    (100, 20, "Speedster", (120, "Achievement Unlocked: Speedster")),
+    (200, 50, "Killer", (250, "Achievement Unlocked: Killer")),
+]
+
+submit_cases = run_cases + [
+    (100, 50, "Unstoppable", (150, "Achievement Unlocked: Unstoppable")),
+    (400, 75, "Gnarly", (475, "Achievement Unlocked: Gnarly")),
+]
+
+
+def test(input1, input2, input3, expected_output):
+    print("---------------------------------")
+    print(f"Inputs: {input1}, {input2}, {input3}")
+    print(f"Expecting: {expected_output}")
+    result = unlock_achievement(input1, input2, input3)
+    print(f"Actual: {result}")
+    if result == expected_output:
+        print("Pass")
+        return True
+    print("Fail")
+    return False
+
+
+def main():
+    passed = 0
+    failed = 0
+    skipped = len(submit_cases) - len(test_cases)
+    for test_case in test_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    if skipped > 0:
+        print(f"{passed} passed, {failed} failed, {skipped} skipped")
+    else:
+        print(f"{passed} passed, {failed} failed")
+
+
+test_cases = submit_cases
+if "__RUN__" in globals():
+    test_cases = run_cases
+
+main()
+
+#skipped the error correction lesson
